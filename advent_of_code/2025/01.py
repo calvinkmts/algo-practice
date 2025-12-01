fc81d5c31fca72a92496ce96f54a0e09e9750509
Calvin Kamtoso <calvinkmts@gmail.com>
2025-12-01 20:19:41 +0700

    feat: update with second part solution

--- advent_of_code/2025/01.py
+++ advent_of_code/2025/01.py
@@ -38,18 +38,42 @@ def second_solution(file_input, starting_digit) -> int:
 
             direction, distance = get_direction_and_distance(line)
 
-            
+            temp = digit_pointed + distance if direction == 'R' else digit_pointed - distance
 
+            old_distance = distance
+            old_digit_pointed = digit_pointed
+
+            if temp > 99 or temp < 1:    
+
+                distance = distance - digit_pointed if direction == 'L' else distance - (100 - digit_pointed)
+
+                digit_pointed = 0
+
+                password += 1
+
+            # password += distance // 100 # add the number of times we have cycled through the digits
+
+            digit_pointed = digit_pointed + distance if direction == 'R' else digit_pointed - distance
+
+            digit_pointed %= 100
+
+            if digit_pointed == 0:
+                password += 1
+
+            # print(f"DEBUG {index}", old_digit_pointed, digit_pointed, password, old_distance, distance)                
 
     return password
 
 
 if __name__ == "__main__":
     
-    input_file = "input.txt"
+    input_file = "input_sample.txt"
 
     digit_pointed = 50
     
-    password = first_solution(input_file, digit_pointed)
+    # password = first_solution(input_file, digit_pointed)
 
-    print(password)
+    old_password = first_solution(input_file, digit_pointed)
+    password = second_solution(input_file, digit_pointed)
+
+    print(old_password, password)
